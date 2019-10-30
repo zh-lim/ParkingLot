c89450a81d4b05c338b55f5a0ae65ba91f549937
zh-lim <zhlim1991@gmail.com>
2019-10-30 11:15:49 +0800

    changes to create parking method

--- app/carpark.py
+++ app/carpark.py
@@ -7,16 +7,15 @@ class Carpark:
     if self.__parkingLots is None:
       self.__numLots = num_lots
       self.__createSlots(self.__numLots)
-      return "Created a parking lot with " + str(self.__numLots) + " slots"
+      return self.__numLots
     else:
-      return "Parking Lots already exists"
+      return -1
+
+  def getNumLots(self):
+    return self.__numLots
 
   def __createSlots(self,num_lots):
     slots = 1
     self.__parkingLots = {}
     for i in range(1,num_lots+1):
       self.__parkingLots[slots] = ""
-
-  def getNumLots(self):
-    return self.__numLots
-
